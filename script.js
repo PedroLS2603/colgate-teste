dbb5bdec225f744567dac6606ed76177a066cd60
Pedro Lima <pedro.l2002@hotmail.com>
2022-01-07 01:08:37 -0300

    fix: mostrando as secções corretamente

--- script.js
+++ script.js
@@ -35,21 +35,11 @@ function showSection(id) {
 function showMoreThanOneSection(origin){
     switch(origin) {            
         case 'regras-resgate-1':
-            let element1 = document.getElementById('regras-resgate-2');
-            element1.style['display'] = 'none';
-                    
-            element1 = document.getElementById('regras-resgate-3');
-            element1.style['display'] = 'none';
             showSection('faq-section-4');
             showSection('faq-section-5');
             showSection('faq-section-6');
             break;
         case 'duvidas-gerais-1':
-            let element = document.getElementById('duvidas-gerais-2');
-            element.style['display'] = 'none';
-                    
-            element = document.getElementById('duvidas-gerais-3');
-            element.style['display'] = 'none';
             showSection('faq-section-7');
             showSection('faq-section-8');
             showSection('faq-section-9');
